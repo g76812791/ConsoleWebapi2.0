f19051c0006136eff8708b766fa37d02833c83ec
wuy <52903234@qq.com>
2016-10-20 15:13:46 +0800

    first

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace ConsoleApplication6
+{
+    internal class Program
+    {
+        public  static void Main()
+        {
+            using (WebApp.Start<Startup>("http://localhost:5000/"))
+            {
+                Console.WriteLine("Press [enter] to quit");
+                Console.ReadLine();
+            }
+        }
+
+       
+    }
+}
---
+++ ConsoleApplication6/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Newtonsoft.Json.Converters;
+using Owin;
+
+namespace ConsoleApplication6
+{
+    public class Startup
+    {
+       
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            appBuilder.UseWebApi(config);
+            // 解决json序列化时的循环引用问题
+            config.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new IsoDateTimeConverter { DateTimeFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss" });
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            // 干掉XML序列化器
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    } 
+}
